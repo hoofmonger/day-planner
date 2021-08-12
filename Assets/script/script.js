df7a77b01fede7e9a1f0486c982128786bb2658a
James Hoofard <james.hoofard.iii@gmail.com>
2021-08-12 14:03:59 -0500

    created some basic variables in Jquery for time and connecting the boxes to the js

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,3 +1,15 @@
+var currentDay = moment().format('dddd') + "" + moment().format('MMMM Do YYYY');
+var currentClockTime = moment().format('h:mm:ss a');
+
+
+var interval = setInterval(function() {
+    var rightNow = moment()
+    $('#currentDay').html(momentNow.format("YYYY MMMM DD") + " "
+    + momentNow.format("dddd")
+    .substring(0,3).toLocaleUpperCase());
+    $("#currentDay"). html(currentDay + " " + momentNow.format("hh:mm:ss A"));
+}, 1000);
+
 var nineHundredHours = document.querySelector("#0900hours")
 var tenHundredHours = document.querySelector("#1000hours")
 var elevenHundredHours = document.querySelector("#1100hours")
@@ -9,3 +21,31 @@ var sixteenHundredHours = document.querySelector("#1600hours")
 var seventeenHundredHours = document.querySelector("#1700hours")
 var eighteenHundredHours = document.querySelector("#1800hours")
 
+function initiatePage() {
+    var initiate0900hours = JSON.parse(localStorage.getItem("0900 am"));
+    nineHundredHours.val(initiate0900hours);
+
+    var initiate0900hours = JSON.parse(localStorage.getItem("0900 am"));
+    nineHundredHours.val(initiate0900hours);
+
+    var initiate0900hours = JSON.parse(localStorage.getItem("0900 am"));
+    nineHundredHours.val(initiate0900hours);
+
+    var initiate0900hours = JSON.parse(localStorage.getItem("0900 am"));
+    nineHundredHours.val(initiate0900hours);
+
+    var initiate0900hours = JSON.parse(localStorage.getItem("0900 am"));
+    nineHundredHours.val(initiate0900hours);
+
+    var initiate0900hours = JSON.parse(localStorage.getItem("0900 am"));
+    nineHundredHours.val(initiate0900hours);
+
+    var initiate0900hours = JSON.parse(localStorage.getItem("0900 am"));
+    nineHundredHours.val(initiate0900hours);
+
+    var initiate0900hours = JSON.parse(localStorage.getItem("0900 am"));
+    nineHundredHours.val(initiate0900hours);
+
+    var initiate0900hours = JSON.parse(localStorage.getItem("0900 am"));
+    nineHundredHours.val(initiate0900hours);
+}
