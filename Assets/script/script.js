7c077f383e48d82098a8ee65531ee434f74a0aeb
James Hoofard <james.hoofard.iii@gmail.com>
2021-08-12 13:17:00 -0500

    added some more html elements and js variables

---
+++ Assets/script/script.js
@@ -0,0 +1,11 @@
+var nineHundredHours = document.querySelector("#0900hours")
+var tenHundredHours = document.querySelector("#1000hours")
+var elevenHundredHours = document.querySelector("#1100hours")
+var tweleveHundredHours = document.querySelector("#1200hours")
+var thirteenHundredHours = document.querySelector("#1300hours")
+var fourteenHundredHours = document.querySelector("#1400hours")
+var fifteenHundredHours = document.querySelector("#1500hours")
+var sixteenHundredHours = document.querySelector("#1600hours")
+var seventeenHundredHours = document.querySelector("#1700hours")
+var eighteenHundredHours = document.querySelector("#1800hours")
+
